0e67dc36a23e90fedb48104063b0a189bbdb2908
Pavlo Vovzhynskyy <106203735+Lluitiy@users.noreply.github.com>
2022-09-07 04:17:02 +0200

    Revert "implemented film list"

--- src/js/film-cards-markup.js
+++ src/js/film-cards-markup.js
@@ -1,35 +1,22 @@
-import {
-  refs
-} from './refs';
-import {
-  MovieApiService
-} from './movie-api-service'
-
-const movieApiService = new MovieApiService;
-
-movieApiService.fetchCards().then(filmsData => filmCardsMarkup(filmsData.results))
+import { refs } from './refs';
 
 export default function filmCardsMarkup(films) {
-  const items = films
-    .map(({
-      genreNames,
-      title,
-      releaseDate = '--',
-      posterPath,
-    }) => {
-      const imageSrc = posterPath ? `https://image.tmdb.org/t/p/w500/${posterPath}` : 'https://via.placeholder.com/395x574'
-      return `
+	const items = films.map(
+		({ title, genre_ids, release_date = '--', poster_path }) => {
+			return `
         <li class="films__item">
             <img 
                 class="films__img" 
-                src=${imageSrc}
+                src="https://image.tmdb.org/t/p/w500/${poster_path}" 
                 alt="${title}">
             <h3 class="films__title">${title}</h3>
-            <p class="films__genre">${genreNames.join(', ')}</p>
-            <p class="films__year">${releaseDate.slice(0, 4)}</p>
+            <p class="films__genre">${genre_ids.join(', ')}</p>
+            <p class="films__year">${release_date.slice(0, 4)}</p>
         </li>
       `;
-    });
+		}
+	);
 
-  refs.filmsUl.innerHTML = items.join('');
+	refs.filmsUl.innerHTML = items.join('');
 }
+console.log(refs.filmsUl);
